f08f2779b2190eb9a456e050fe08bf95f1d23411
nporpeccop <40239823+nporpeccop@users.noreply.github.com>
2018-06-22 14:02:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-let gotSum = Number (prompt ("Сколько у вас денег?"));
-let applesBought = Number (prompt ("Сколько яблок?"));
-let breadsBought = Number (prompt ("Сколько булок?"));
-let applesPrice = Number (prompt ("Цена яблока?")) * applesBought;
-let breadsPrice = Number (prompt ("Цена булки?")) * breadsBought;
-let enoughMoney = applesPrice + breadsPrice <= gotSum;
-document.body.innerHtml = "<p>" + String (enoughMoney) + "</p>"
+console.log ("You are at: " + window.location);
+
+let num = parseInt (prompt ("Enter a number:"));
+if (num < 0) num = -num;
+console.log (num)
